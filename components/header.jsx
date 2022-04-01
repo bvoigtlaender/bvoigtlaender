35835bf2d18f8c49a2f56eda4c460b79c1c7283f
Bjarne Voigtländer <bjarne.voigtlaender@icloud.com>
2022-04-01 15:41:47 +0200

    styling changes

--- components/header.jsx
+++ components/header.jsx
@@ -2,12 +2,12 @@ import NavLink from './navlink';
 
 export default function Header() {
   return (
-    <div className="mb-4 flex border-4 border-solid border-neutral-800 bg-white shadow-backdrop">
+    <div className="mb-4 flex overflow-hidden border-4 border-solid border-neutral-800 bg-white shadow-backdrop">
       <NavLink href={'/'}>stuff</NavLink>
       <NavLink href={'/websites'}>websites</NavLink>
       <NavLink href={'/games'}>games</NavLink>
       <NavLink href={'/about'}>about me</NavLink>
-      <p className="ml-auto flex cursor-default items-center bg-white py-4 px-8 font-abril text-2xl font-bold">
+      <p className="ml-auto flex cursor-default items-center bg-white py-2 px-4 font-abril text-2xl font-bold sm:py-4 sm:px-8">
         made by bjarne
       </p>
     </div>
--- components/navlink.jsx
+++ components/navlink.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NavLink({ href, children }) {
   return (
     <Link href={href}>
-      <a className="group flex cursor-pointer items-center bg-white py-4 px-8 hover:bg-black">
+      <a className="group flex cursor-pointer items-center bg-white py-2 px-4 hover:bg-black sm:py-4 sm:px-8">
         <p className="font-abril text-2xl group-hover:text-white">{children}</p>
       </a>
     </Link>
